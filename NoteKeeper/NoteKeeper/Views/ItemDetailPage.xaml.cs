b5527feae4c5e74a182349457b77da594c21e0a6
Enzo Joaquin Itona Erfe <enzo@mysoftinn.com>
2021-03-23 16:35:30 +0800

    Initial Commit +Added Main Folders (xaml + cs)

---
+++ NoteKeeper/NoteKeeper/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using NoteKeeper.Models;
+using NoteKeeper.ViewModels;
+using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace NoteKeeper.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        ItemDetailViewModel viewModel;
+        public Note Note { get; set; }
+
+        public ItemDetailPage(ItemDetailViewModel viewModel)
+        {
+            InitializeComponent();
+            InitializeData();
+            BindingContext = Note;
+        }
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            //this.NoteHeading=
+            InitializeData();
+            //BindingContext = new ItemDetailViewModel();
+            BindingContext = Note;
+        }
+        void InitializeData()
+        {
+            Note = new Note { Heading="Test Note", Text="Text for test note"};
+        }
+        public void Cancel_Clicked(object sender, EventArgs eventArgs)
+        {
+            DisplayAlert("Cancel option", "Cancel was selected", "Button 2", "Button 1");
+        }
+
+        public void Save_Clicked(object sender, EventArgs eventArgs)
+        {
+            DisplayAlert("Save option", "Save was clicked", "Button 2", "Button 1");
+        }
+    }
+}
