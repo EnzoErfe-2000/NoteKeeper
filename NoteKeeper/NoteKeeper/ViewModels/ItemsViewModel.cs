146d355810869b7c1437316946d1b59bdc846987
Enzo Joaquin Itona Erfe <enzo@mysoftinn.com>
2021-03-24 11:50:09 +0800

    Started Chap 7 +Added Notes to startup of ItemsPage

--- NoteKeeper/NoteKeeper/ViewModels/ItemsViewModel.cs
+++ NoteKeeper/NoteKeeper/ViewModels/ItemsViewModel.cs
@@ -12,37 +12,39 @@ namespace NoteKeeper.ViewModels
 {
     public class ItemsViewModel : BaseViewModel
     {
-        public ObservableCollection<Item> Items { get; set; }
+        public ObservableCollection<Note> Notes { get; set; }
         public Command LoadItemsCommand { get; set; }
 
         public ItemsViewModel()
         {
             Title = "Browse";
-            Items = new ObservableCollection<Item>();
+            Notes = new ObservableCollection<Note>();
             LoadItemsCommand = new Command(async () => await ExecuteLoadItemsCommand());
 
+            /*
             MessagingCenter.Subscribe<NewItemPage, Item>(this, "AddItem", async (obj, item) =>
             {
                 var newItem = item as Item;
                 Items.Add(newItem);
                 await DataStore.AddItemAsync(newItem);
             });
+            */
         }
 
         async Task ExecuteLoadItemsCommand()
         {
             if (IsBusy)
                 return;
-
+            //IsBusy ensures no further loads are done
             IsBusy = true;
 
             try
             {
-                Items.Clear();
-                var items = await DataStore.GetItemsAsync(true);
-                foreach (var item in items)
+                Notes.Clear();
+                var notes = await PluralsightDataStore.GetNotesAsync();
+                foreach (var note in notes)
                 {
-                    Items.Add(item);
+                    Notes.Add(note);
                 }
             }
             catch (Exception ex)
